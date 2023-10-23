8658cd3e186a1b6b147beab8feb27948fff6de64
AndrewMysliuk <andre061120@gmail.com>
2023-10-23 14:01:19 +0300

    fix: model folder

--- src/models/IRequests.ts
+++ src/models/IRequests.ts
@@ -1,14 +1,13 @@
 import { Request } from "express"
 import { JwtPayload } from "jsonwebtoken"
 import { uuid } from "./ICommon"
-import { IUserResponse } from "./IUser"
 
 export interface IUserJwtPayload extends JwtPayload {
   id?: uuid
 }
 
 export interface IDataRequest extends Request {
-  user?: IUserResponse | IUserJwtPayload
+  user?: string | IUserJwtPayload
   file_path?: string
   static_path?: string
 }
